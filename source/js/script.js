2df9bea862e9fb95a68c047e919f22dbda1926a0
Core2-0 <Core2-00@mail.ru>
2021-10-25 17:19:59 +0500

    Добавляет ретинизацию, изображения для разных экранов на главной странице

--- source/js/script.js
+++ source/js/script.js
@@ -17,20 +17,26 @@ navToggle.onclick = () => {
 };
 
 // Slider
-const sliderImageBefore = document.querySelector('.slider__image-wrapper img:first-child');
-const sliderImageAfter = document.querySelector('.slider__image-wrapper img:last-child');
+const sliderImageBefore = document.querySelector('.slider__cat-before .slider__image');
+const sliderImageAfter = document.querySelector('.slider__cat-after .slider__image');
 const beforeButton = document.querySelector('.slider__before');
 const afterButton = document.querySelector('.slider__after');
 
 console.log(sliderImageBefore);
 console.log(sliderImageAfter);
 
+beforeButton.setAttribute('disabled', 'disabled');
+
 afterButton.onclick = () => {
   sliderImageAfter.classList.add('slider__image--show');
   sliderImageBefore.classList.remove('slider__image--show');
+  afterButton.setAttribute('disabled', 'disabled');
+  beforeButton.removeAttribute('disabled');
 }
 
 beforeButton.onclick = () => {
   sliderImageBefore.classList.add('slider__image--show');
   sliderImageAfter.classList.remove('slider__image--show');
+  beforeButton.setAttribute('disabled', 'disabled');
+  afterButton.removeAttribute('disabled');
 }
