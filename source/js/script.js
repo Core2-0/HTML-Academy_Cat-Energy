cd6b0d2f61342dba8093534dd138bc4d5651aa94
Core2-0 <Core2-00@mail.ru>
2021-10-25 15:05:41 +0500

    Добавляет прототип работы слайдера

--- source/js/script.js
+++ source/js/script.js
@@ -15,3 +15,22 @@ navToggle.onclick = () => {
   navToggle.classList.toggle('main-nav__toggle-menu--opened');
   navToggle.classList.toggle('main-nav__toggle-menu--closed');
 };
+
+// Slider
+const sliderImageBefore = document.querySelector('.slider__image-wrapper img:first-child');
+const sliderImageAfter = document.querySelector('.slider__image-wrapper img:last-child');
+const beforeButton = document.querySelector('.slider__before');
+const afterButton = document.querySelector('.slider__after');
+
+console.log(sliderImageBefore);
+console.log(sliderImageAfter);
+
+afterButton.onclick = () => {
+  sliderImageAfter.classList.add('slider__image--show');
+  sliderImageBefore.classList.remove('slider__image--show');
+}
+
+beforeButton.onclick = () => {
+  sliderImageBefore.classList.add('slider__image--show');
+  sliderImageAfter.classList.remove('slider__image--show');
+}
