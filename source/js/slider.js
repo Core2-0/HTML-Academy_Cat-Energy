5994a44fe2a8ea92ede4f4a47f0f4a9277419de4
Core2-0 <Core2-00@mail.ru>
2021-11-17 10:35:12 +0500

    Slider v2.1.0

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,66 +1,65 @@
-const clientWidth = document.body.clientWidth;
-const tabletWidth = 768;
+const mobileWidth = window.matchMedia('(max-width: 767px');
+const tabletWidth = window.matchMedia('(min-width: 768px)');
 
 // Slider
 const progress = document.querySelector('.slider');
-const imageAfter = document.querySelector('.slider__image-after');
-const imageBefore = document.querySelector('.slider__image-before');
+const imageAfter = progress.querySelector('.slider__image-after');
+const imageBefore = progress.querySelector('.slider__image-before');
 const scale = progress.querySelector('.slider__range');
 const range = scale.querySelector('.slider__range-input');
 const rangeToggle = scale.querySelector('.slider__range-toggle');
+const btnBefore = progress.querySelector('.slider__button--before');
+const btnAfter = progress.querySelector('.slider__button--after');
 
-console.log(imageAfter.classList.contains('slider__image-after--active'));
-console.log(imageBefore.classList.contains('slider__image-before--active'));
+btnBefore.addEventListener('click', function () {
+  range.value = 0;
+  range.setAttribute('value', range.value);
+  imageBefore.style.width = '100%';
+  imageAfter.style.width = '0%';
 
-if (progress) {
-  const btnBefore = progress.querySelector('.slider__button--before');
-  const btnAfter = progress.querySelector('.slider__button--after');
+  if (mobileWidth.matches) {
+    rangeToggle.style.transform = 'translateX(0%)';
+  }
+});
 
-  btnBefore.addEventListener('click', function () {
-    if (imageAfter.classList.contains('slider__image-after--active')) {
-      console.log('click');
-      imageAfter.classList.remove('slider__image-after--active');
-      imageBefore.classList.add('slider__image-before--active');
-      imageBefore.style.width = `${100}%`;
-      imageAfter.style.width = `${0}%`;
-      rangeToggle.style.transform = 'translateX(0%)';
-    }
-  })
+btnAfter.addEventListener('click', function () {
+  range.value = 100;
+  range.setAttribute('value', range.value);
+  imageBefore.style.width = '0%';
+  imageAfter.style.width = '100%';
 
-  btnAfter.addEventListener('click', function () {
-    if (imageBefore.classList.contains('slider__image-before--active')) {
-      console.log('click');
-      imageBefore.classList.remove('slider__image-before--active');
-      imageAfter.classList.add('slider__image-after--active');
-      imageAfter.style.width = `${100}%`;
-      imageBefore.style.width = `${0}%`;
-      rangeToggle.style.transform = 'translateX(100%)';
-    }
-  })
+  if (mobileWidth.matches) {
+    rangeToggle.style.transform = 'translateX(100%)';
+  }
+});
 
-  if (clientWidth >= tabletWidth) {
+function handleMobile(media) {
+  if (media.matches) {
+    range.value = 0;
+    range.setAttribute('value', range.value);
 
-    range.value = '50%';
-
-    range.addEventListener('input', function () {
-      // console.log((100 - range.value) + '%');
-      // rangeToggle.style.left = `${0 + range.value}%`;
-      imageBefore.style.width = `${100 - range.value}%`
-      imageAfter.style.width = `${range.value}%`;
-    })
-
-    btnBefore.addEventListener('click', function () {
-      range.value = 1;
-      // rangeToggle.style.left = `${0}%`;
-      imageBefore.style.width = '100%';
-      imageAfter.style.width = '0%';
-    });
-
-    btnAfter.addEventListener('click', function () {
-      range.value = 100;
-      // rangeToggle.style.left = `${100}%`;
-      imageBefore.style.width = '0%';
-      imageAfter.style.width = '100%';
-    });
+    imageBefore.style.width = `${100 - range.value}%`;
+    imageAfter.style.width = `${range.value}%`;
   }
 }
+
+function handleTablet(media) {
+  if (media.matches) {
+    range.value = 50;
+    range.setAttribute('value', range.value);
+
+    imageBefore.style.width = `${range.value}%`;
+    imageAfter.style.width = `${range.value}%`;
+
+    range.addEventListener('input', function () {
+      this.setAttribute('value', this.value);
+      imageBefore.style.width = `${100 - range.value}%`;
+      imageAfter.style.width = `${range.value}%`;
+    })
+  }
+}
+
+mobileWidth.addListener(handleMobile);
+handleMobile(mobileWidth);
+tabletWidth.addListener(handleTablet);
+handleTablet(tabletWidth);
