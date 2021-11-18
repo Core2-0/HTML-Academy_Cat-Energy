0b6371bb1ffafe357f711f3edb920554c449cb43
Core2-0 <Core2-00@mail.ru>
2021-11-18 14:05:40 +0500

    Добавляет favicon

--- gulpfile.js
+++ gulpfile.js
@@ -105,6 +105,7 @@ const copy = (done) => {
   gulp.src([
     "source/fonts/*.{woff2,woff}",
     "source/*.ico",
+    "source/manifest.webmanifest",
     "source/img/**/*.svg",
     "!source/img/icons/*.svg",
   ], {
