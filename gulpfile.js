16fff05d9df298435ac00a72c2fbd39fa48b0809
Core2-0 <Core2-00@mail.ru>
2021-11-26 16:07:38 +0500

    Fixed custom card

--- gulpfile.js
+++ gulpfile.js
@@ -150,7 +150,7 @@ const reload = (done) => {
 
 const watcher = () => {
   gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
-  gulp.watch("source/js/script.js", gulp.series(scripts));
+  gulp.watch("source/js/*.js", gulp.series(scripts));
   gulp.watch("source/*.html", gulp.series(html, reload));
 }
 
